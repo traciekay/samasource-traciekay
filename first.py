5ed119e4bba854ab8966a631b3b1d17c8c55ae5a
Tracy Kaonje Omamo <tomamo@SamaDc.corp>
2017-10-30 16:14:42 +0300

    Tests running successfully

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 def divide_number(num1, num2):
    try:
-		return (num1/num2)
-	except ZeroDivisionError:
-		print("Cannot Divide by zero")
+       return (num1/num2)
+   except ZeroDivisionError:
+       print("Cannot Divide by zero")
